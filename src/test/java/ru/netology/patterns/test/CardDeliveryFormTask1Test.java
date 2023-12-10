b1e74d73013af567c28fe2f2dc0d91c27d0c744f
Irina <izhts1482@gmail.com>
2023-12-10 21:01:04 +0300

    Allure added

--- src/test/java/ru/netology/patterns/test/CardDeliveryFormTask1Test.java
+++ src/test/java/ru/netology/patterns/test/CardDeliveryFormTask1Test.java
@@ -1,6 +1,10 @@
 package ru.netology.patterns.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.patterns.data.DataGenerator;
@@ -12,6 +16,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTask1Test {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @Test
     void shouldChangeDate() {
         open("http://localhost:9999/");
